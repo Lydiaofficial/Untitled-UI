390a7a3466489750fe14f032e02d90f059a1282a
Lydia Amadi Chinyere <Lydiaamadichinyere@gmail.com>
2024-03-24 14:43:58 +0100

    updated files for mobile responsiveness

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+const menuIcon = document.getElementById('menu-toggle')
+const dropdownMenu = document.getElementById('dropdown-menu')
+
+menuIcon.addEventListener('click', function() {
+    dropdownMenu.style.display = dropdownMenu.style.display === 'none' ? 'block' : 'none';
+    console.log('okay na')
+})
